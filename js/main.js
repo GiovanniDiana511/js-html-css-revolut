4bfc3baf19cced3d25ae92aea71571016caea5cb
Giovanni <giovanni.diana511@gmail.com>
2020-02-29 18:48:51 +0100

    Da rivedere. Alcune funzionalità non presenti.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    
+    $(".menu-item.icon").click(function(){
+        $(this).find(".dropdown-menu").slideToggle(500);
+        $(".menu-item.icon").addClass('ruota');
+    });
 });
